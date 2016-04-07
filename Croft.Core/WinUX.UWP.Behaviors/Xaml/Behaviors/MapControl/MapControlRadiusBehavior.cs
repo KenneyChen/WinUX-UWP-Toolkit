86e530caa3449a1a0b37ece69c6486e0b1b91c4f
James Croft <jamz_c@hotmail.co.uk>
2016-04-07 09:58:00 +0100

    Added MapControl radius drawing behavior which will draw a radius at a given center point. Also added Geopoint extensions to support the radius calculation.

---
+++ Croft.Core/WinUX.UWP.Behaviors/Xaml/Behaviors/MapControl/MapControlRadiusBehavior.cs
@@ -0,0 +1,111 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MapControlRadiusBehavior.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft.
+// </copyright>
+// <summary>
+//   Defines the MapControlRadiusBehavior type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinUX.Xaml.Behaviors.MapControl
+{
+    using System.Linq;
+
+    using Windows.Devices.Geolocation;
+    using Windows.UI;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls.Maps;
+
+    using Microsoft.Xaml.Interactivity;
+
+    using WinUX.Extensions;
+
+    /// <summary>
+    /// A behavior to draw a radius on the <see cref="MapControl"/>.
+    /// </summary>
+    public class MapControlRadiusBehavior : Behavior
+    {
+        private MapControl MapControl => this.AssociatedObject as MapControl;
+
+        public static readonly DependencyProperty CenterProperty = DependencyProperty.Register(
+            nameof(Center),
+            typeof(Geopoint),
+            typeof(MapControlRadiusBehavior),
+            new PropertyMetadata(null, OnCenterChanged));
+
+        public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register(
+            "Radius",
+            typeof(double),
+            typeof(MapControlRadiusBehavior),
+            new PropertyMetadata(0.0, OnRadiusChanged));
+
+        public Color RadiusFillColor { get; set; }
+
+        public Color RadiusBorderColor { get; set; }
+
+        private static void OnRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as MapControlRadiusBehavior;
+            behavior?.UpdateRadius();
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return (double)this.GetValue(RadiusProperty);
+            }
+            set
+            {
+                this.SetValue(RadiusProperty, value);
+            }
+        }
+
+        private MapPolygon MapRadius { get; set; }
+
+        private static void OnCenterChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            var behavior = d as MapControlRadiusBehavior;
+            behavior?.UpdateRadius();
+        }
+
+        private void UpdateRadius()
+        {
+            if (this.MapControl != null && this.Center != null)
+            {
+                var radiusCirclePoints = this.Center.GetCirclePoints(this.Radius);
+
+                if (this.MapRadius != null)
+                {
+                    var element = this.MapControl.MapElements.FirstOrDefault(x => x.Equals(this.MapRadius));
+                    if (element != null)
+                    {
+                        this.MapControl.MapElements.Remove(element);
+                    }
+                }
+
+                this.MapRadius = new MapPolygon
+                                     {
+                                         Path = new Geopath(radiusCirclePoints),
+                                         ZIndex = 0,
+                                         FillColor = this.RadiusFillColor,
+                                         StrokeColor = this.RadiusBorderColor
+                                     };
+
+                this.MapControl.MapElements.Add(this.MapRadius);
+            }
+        }
+
+        public Geopoint Center
+        {
+            get
+            {
+                return (Geopoint)this.GetValue(CenterProperty);
+            }
+            set
+            {
+                this.SetValue(CenterProperty, value);
+            }
+        }
+    }
+}
---
+++ Croft.Core/WinUX.UWP.Core/Extensions/GeopointExtensions.cs
@@ -0,0 +1,98 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GeopointExtensions.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft.
+// </copyright>
+// <summary>
+//   A collection of <see cref="Geopoint" /> extensions to support <see cref="MapControl" />.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinUX.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Windows.Devices.Geolocation;
+
+    /// <summary>
+    /// A collection of <see cref="Geopoint"/> extensions to support <see cref="Windows.UI.Xaml.Controls.Maps.MapControl"/>.
+    /// </summary>
+    public static class GeopointExtensions
+    {
+        /// <summary>
+        /// Gets a point at a given distance and bearing from another.
+        /// </summary>
+        /// <param name="point">
+        /// The point.
+        /// </param>
+        /// <param name="distance">
+        /// The distance.
+        /// </param>
+        /// <param name="bearing">
+        /// The bearing.
+        /// </param>
+        /// <returns>
+        /// Returns a <see cref="BasicGeoposition"/> value.
+        /// </returns>
+        public static BasicGeoposition GetPointAtDistanceAndBearing(
+            this Geopoint point,
+            double distance,
+            double bearing)
+        {
+            const double DegreesToRadian = Math.PI / 180.0;
+            const double RadianToDegrees = 180.0 / Math.PI;
+            const double EarthRadius = 6378137.0;
+
+            var latA = point.Position.Latitude * DegreesToRadian;
+            var lonA = point.Position.Longitude * DegreesToRadian;
+            var angularDistance = distance / EarthRadius;
+            var trueCourse = bearing * DegreesToRadian;
+
+            var lat =
+                Math.Asin(
+                    Math.Sin(latA) * Math.Cos(angularDistance)
+                    + Math.Cos(latA) * Math.Sin(angularDistance) * Math.Cos(trueCourse));
+
+            var dlon = Math.Atan2(
+                Math.Sin(trueCourse) * Math.Sin(angularDistance) * Math.Cos(latA),
+                Math.Cos(angularDistance) - Math.Sin(latA) * Math.Sin(lat));
+
+            var lon = ((lonA + dlon + Math.PI) % (Math.PI * 2)) - Math.PI;
+
+            var result = new BasicGeoposition { Latitude = lat * RadianToDegrees, Longitude = lon * RadianToDegrees };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a collection of points representing a circle around a given point.
+        /// </summary>
+        /// <param name="center">
+        /// The center point.
+        /// </param>
+        /// <param name="radius">
+        /// The radius.
+        /// </param>
+        /// <param name="numberOfPoints">
+        /// The number of points to generate.
+        /// </param>
+        /// <returns>
+        /// Returns a collection of <see cref="BasicGeoposition"/> values representing a circle.
+        /// </returns>
+        public static IEnumerable<BasicGeoposition> GetCirclePoints(
+            this Geopoint center,
+            double radius,
+            int numberOfPoints = 180)
+        {
+            var angle = 360.0 / numberOfPoints;
+            var locations = new List<BasicGeoposition>();
+
+            for (var i = 0; i <= numberOfPoints; i++)
+            {
+                locations.Add(center.GetPointAtDistanceAndBearing(radius, angle * i));
+            }
+
+            return locations;
+        }
+    }
+}
