46109d1dfe1a1968c83f502568f985ba5ff86731
James Croft <jamz_c@hotmail.co.uk>
2016-04-13 20:41:25 +0100

    Added button click behavior. Contains an ActionCollection which executes them when the attached button is clicked.

---
+++ WinUX/WinUX.UWP.Behaviors/Xaml/Behaviors/Button/ButtonClickedBehavior.cs
@@ -0,0 +1,56 @@
+﻿namespace WinUX.Xaml.Behaviors.Button
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    using Microsoft.Xaml.Interactivity;
+
+    public class ButtonClickedBehavior : Behavior
+    {
+        public Button Button => this.AssociatedObject as Button;
+
+        public static readonly DependencyProperty ActionsProperty = DependencyProperty.Register(
+            "Actions",
+            typeof(ActionCollection),
+            typeof(ButtonClickedBehavior),
+            new PropertyMetadata(default(ActionCollection)));
+
+        public ActionCollection Actions
+        {
+            get
+            {
+                var actions = (ActionCollection)this.GetValue(ActionsProperty);
+                if (actions != null)
+                {
+                    return actions;
+                }
+
+                actions = new ActionCollection();
+                this.SetValue(ActionsProperty, actions);
+
+                return actions;
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            if (this.Button != null)
+            {
+                this.Button.Click += this.OnButtonClicked;
+            }
+        }
+
+        private void OnButtonClicked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Interaction.ExecuteActions(this.Button, this.Actions, routedEventArgs);
+        }
+
+        protected override void OnDetaching()
+        {
+            if (this.Button != null)
+            {
+                this.Button.Click -= this.OnButtonClicked;
+            }
+        }
+    }
+}
