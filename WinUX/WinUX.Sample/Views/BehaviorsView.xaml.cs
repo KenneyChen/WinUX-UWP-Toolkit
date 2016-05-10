fd074240259dbf30aa565295114e37cee2d493e0
James Croft <jamz_c@hotmail.co.uk>
2016-05-10 12:55:50 +0100

    Updated behaviors samples

--- WinUX/WinUX.Sample/Views/BehaviorsView.xaml.cs
+++ WinUX/WinUX.Sample/Views/BehaviorsView.xaml.cs
@@ -1,23 +1,61 @@
-﻿namespace WinUX.Sample.Views
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BehaviorsView.xaml.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft.
+// </copyright>
+// <summary>
+//   Defines the view for demonstrating the use of WinUX behaviors.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WinUX.Sample.Views
 {
-    using Windows.UI.Xaml.Controls;
+    using System;
 
-    using WinUX.Sample.ViewModels;
+    using Windows.Storage.Pickers;
+    using Windows.UI.Xaml;
 
+    /// <summary>
+    /// Defines the view for demonstrating the use of WinUX behaviors.
+    /// </summary>
     public sealed partial class BehaviorsView
     {
-        private BehaviorsViewModel viewModel;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Sample.MainPage"/> class.
         /// </summary>
         public BehaviorsView()
         {
             this.InitializeComponent();
-
-            this.DataContext = this.ViewModel;
         }
 
-        public BehaviorsViewModel ViewModel => this.viewModel ?? (this.viewModel = new BehaviorsViewModel());
+        private async void OnFileThumbnailButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var openPicker = new FileOpenPicker
+                                 {
+                                     ViewMode = PickerViewMode.List,
+                                     CommitButtonText = "Open file",
+                                     SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+                                 };
+
+            openPicker.FileTypeFilter.Add(".txt");
+            openPicker.FileTypeFilter.Add(".docx");
+            openPicker.FileTypeFilter.Add(".pdf");
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".ini");
+            openPicker.FileTypeFilter.Add(".psd");
+            openPicker.FileTypeFilter.Add(".exe");
+            openPicker.FileTypeFilter.Add(".ppt");
+            openPicker.FileTypeFilter.Add(".zip");
+            openPicker.FileTypeFilter.Add(".vsix");
+            openPicker.FileTypeFilter.Add(".nupkg");
+            openPicker.FileTypeFilter.Add(".msi");
+
+
+            var file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                this.FileThumbnailBehavior.File = file;
+            }
+        }
     }
 }
